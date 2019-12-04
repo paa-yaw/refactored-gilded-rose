d391d1d3b708a4ae751bdfa12a574766a3bed2aa
Derek Owusu-Frimpong <deepsky_5@live.com>
2019-12-04 17:11:30 +0000

    further refactoring making use of hash

--- behaviour.rb
+++ behaviour.rb
@@ -1,26 +1,27 @@
 module Behaviour
     class ItemWrapper 
       attr_accessor :item 
+
+      ITEM_BEHAVIOUR_HASH = {
+        'Aged Brie' => 'aged_brie',
+        'Backstage passes to a TAFKAL80ETC concert' => 'backstage_pass',
+        'Sulfuras, Hand of Ragnaros' => 'sulfuras',
+        'Conjured' => 'conjured'
+      }
   
       def initialize(item)
         @item = item
       end
-  
+      
       def behaviour
-        case item.name
-        when 'Aged Brie'
-          aged_brie_behaviour(item)
-        when 'Backstage passes to a TAFKAL80ETC concert'
-          backstage_pass_behaviour(item)
-        when 'Sulfuras, Hand of Ragnaros'
-          sulfuras_behaviour(item)
-        when  'Conjured'
-          conjured_behaviour(item)
-        else
+        case item.name 
+        when ITEM_BEHAVIOUR_HASH.key(ITEM_BEHAVIOUR_HASH[item.name])
+        __send__("#{ITEM_BEHAVIOUR_HASH[item.name]}_behaviour".to_sym, item)
+        else 
           normal_behaviour(item)
         end
       end
-  
+
       private
   
       def normal_behaviour(item)
